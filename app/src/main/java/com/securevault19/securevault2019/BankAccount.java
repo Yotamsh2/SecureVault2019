18bf12b36823e1536a29774b63d21c362757888d
Oriech90 <53427355+Oriech90@users.noreply.github.com>
2019-08-15 16:36:25 +0300

    Add files via upload

---
+++ app/src/main/java/com/securevault19/securevault2019/BankAccount.java
@@ -0,0 +1,164 @@
+package com.example.securevault19self.Record;
+import androidx.room.PrimaryKey;
+import androidx.room.Entity;
+
+
+@Entity
+public class BankAccount extends Record {
+
+    @PrimaryKey(autoGenerate = true)
+    public int recordID;
+
+    //exclusive variables
+    private String userName;
+    private long IBAN;
+    private int accountNumber;
+    private int bankNumber;
+    private String address;
+
+    public BankAccount(){
+        setTitle(title);
+        setUserName(userName);
+        setPassword(password);
+        setAccountNumber(accountNumber);
+        setIBAN(IBAN);
+        setBankNumber(bankNumber);
+        setAddress(address);
+        setExpiringDate(expiringDate);
+    }
+
+    //setters
+    public void setRecordID(int recordID) {
+        this.recordID = recordID;
+    }
+
+    public void setIBAN(long IBAN) {
+        //invokes String from suitable text field
+        this.IBAN = IBAN;
+    }
+
+    public void setUserName(String userName) {
+        //invokes String from suitable text field
+        this.userName = userName;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        //invokes String from suitable text field
+        this.accountNumber = accountNumber;
+    }
+
+    public void setBankNumber(int bankNumber) {
+        //invokes String from suitable text field
+        this.bankNumber = bankNumber;
+    }
+
+    public void setAddress(String address) {
+        //invokes String from suitable text field
+        this.address = address;
+    }
+
+    //override setters
+    @Override
+    public void setTitle(String title) {
+         this.title = title;
+    }
+
+    @Override
+    public void setPassword(String password) {
+        //sends to complicated functions(Cryptographic module)
+    }
+
+    @Override
+    public void setWebsite(String website) {
+        //invokes String from suitable text field
+    }
+
+    @Override
+    public void setEmail(String email) {
+        //invokes String from suitable text field
+    }
+
+    @Override
+    public void setExpiringDate(String expiringDate) {
+        //by whatever way we choose
+    }
+
+    public void setDateCreated(String dateCreated){}
+
+    public void setLastModified(String lastModified){}
+
+    public void setCategory(String category){} //NO NEED
+
+
+    //getters
+    public String getUserName() {
+        return userName;
+    }
+
+    public long getIBAN() {
+        return IBAN;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public int getBankNumber() {
+        return bankNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+
+    //override getters
+    @Override
+    public int getRecord_ID() {
+        return recordID;
+    }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getWebsite() {
+        return website;
+    }
+
+    @Override
+    public String getEmail() { return email;  }
+
+    @Override
+    public String getExpiringDate() {
+        return expiringDate;
+    }
+
+    @Override
+    public String getDateCreated(){return dateCreated; }
+
+    @Override
+    public String getLastModified(){ return lastModified; }
+
+    @Override
+    public String getCategory(){ return category; }
+
+
+
+    @Override
+    public void addTextInbox() {
+
+    }
+
+    @Override
+    public void addCategory() {
+
+    }
+}
---
+++ app/src/main/java/com/securevault19/securevault2019/Record.java
@@ -0,0 +1,61 @@
+package com.example.securevault19self.Record;
+
+public abstract class Record {
+
+    public int recordID = 0;
+    public String title = null;
+    public String category = null;
+    public String dateCreated = null; //create dateCreated class and make a toString method.
+    public String lastModified = null; //create lastModified class and make a toString method.
+    public String password = null;
+    public String email = null;
+    public String website = null;
+    public String expiringDate = null; //addition to the class diagram
+
+    public abstract void setRecordID(int recordID);
+    public abstract void setTitle(String title); //void instead of 'int'
+    public abstract void setPassword(String password); ////void instead of 'int'
+    public abstract void setWebsite(String website); //void instead of 'int'
+    public abstract void setEmail(String email); //void instead of 'int'
+    public abstract void setExpiringDate(String expiringDate); //addition to the class diagram
+    public abstract void setDateCreated(String dateCreated);
+    public abstract void setLastModified(String lastModified);
+    public abstract void setCategory(String category);
+
+    public abstract int getRecord_ID();
+    public abstract String getTitle();
+    public abstract String getPassword();
+    public abstract String getWebsite();
+    public abstract String getEmail();
+    public abstract String getExpiringDate();
+    public abstract String getDateCreated();
+    public abstract String getLastModified();
+    public abstract String getCategory();
+
+
+    public abstract void addCategory(); //void instead of 'int'
+    public abstract void addTextInbox(); //void instead of 'int'
+    // (because we are not using the notifications list for the returning
+}
+
+ /*   MOVE TO RecordDAO
+    //@Query(*to cpmplete*)
+    void readFromRecord(); //void instead of 'string'
+    @Insert
+    void createRecord();
+    @Update
+    void updateRecord();
+    @Delete
+    void deleteRecord();
+
+    //@Query(*to cpmplete*)
+     String checkWhenDataCreated(); //for example: 18/12/2019
+    //@Query(*to cpmplete*)
+    String checkWhenLastModified(); //for example: 18/12/2019
+    //@Query(*to cpmplete*)
+     String checkModifiedFrom(); //returns which device made the modification
+
+    @Update void changePassword(); //void instead of 'int'
+    @Update void changeEmail(); //void instead of 'int'
+
+    */
