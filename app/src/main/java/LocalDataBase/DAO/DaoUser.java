87b9f5288e42963d38c3bfcb4d6c8d41c67399e1
Ivgesha <evgenise93@gmail.com>
2019-08-28 15:33:37 +0300

    uploded the Repository

--- app/src/main/java/LocalDataBase/DAO/DaoUser.java
+++ app/src/main/java/LocalDataBase/DAO/DaoUser.java
@@ -6,7 +6,7 @@ import androidx.room.Insert;
 import androidx.room.Query;
 import androidx.room.Update;
 
-import com.securevault19.securevault2019.Record.BankAccount;
+
 import com.securevault19.securevault2019.User.User;
 
 import java.util.List;
