b7d830f26292bfac9e056c4a8f6e2ce6b43f5fe5
Yotamsh2 <yotamsh2@gmail.com>
2019-08-28 13:25:57 +0300

    איקון

--- app/src/main/java/com/securevault19/securevault2019/MainActivity.java
+++ app/src/main/java/com/securevault19/securevault2019/MainActivity.java
@@ -11,7 +11,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 @SuppressLint("Registered")
 public class MainActivity extends AppCompatActivity {
