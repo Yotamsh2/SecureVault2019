2cbe5ff4f456aa45365edd131b1d9de8315082ac
Ivgesha <evgenise93@gmail.com>
2019-08-28 14:35:00 +0300

    added DaoUser

---
+++ app/src/main/java/LocalDataBase/DAO/DaoUser.java
@@ -0,0 +1,31 @@
+package LocalDataBase.DAO;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.securevault19.securevault2019.Record.BankAccount;
+import com.securevault19.securevault2019.User.User;
+
+import java.util.List;
+
+public interface DaoUser {
+
+    @Insert
+    void insert(User user);
+
+    @Update
+    void update(User user);
+
+    @Delete
+    void delete(User user);
+
+    @Query("DELETE FROM user_table")
+    void deleteAllRecords();
+
+    @Query("SELECT * FROM user_table ORDER BY priority DESC")
+    LiveData<List<BankAccount>> getAllRecords();
+
+}
