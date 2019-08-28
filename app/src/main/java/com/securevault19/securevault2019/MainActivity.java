d919dd6c168511bf0a0ade3850cc0064cdb6f44e
ORIE <oriech90@gmail.com>
2019-08-28 13:17:48 +0300

    MainActivity post a Toast and doesn't crash

--- app/src/main/java/com/securevault19/securevault2019/MainActivity.java
+++ app/src/main/java/com/securevault19/securevault2019/MainActivity.java
@@ -11,22 +11,48 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 @SuppressLint("Registered")
 public class MainActivity extends AppCompatActivity {
 
-    private EditText userName;
-    private EditText password;
-    private Button signIn ;
-    private int counter ;
+//    private EditText userName;
+//    private EditText password;
+//    private Button signIn ;
+   private int counter ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-         EditText userName = findViewById(R.id.userName);
-         EditText password = findViewById(R.id.password);
-         Button signIn = findViewById(R.id.signIn);
-         counter = 3;
+
+         final EditText userName = findViewById(R.id.userName);
+         final EditText password = findViewById(R.id.password);
+         final Button buttontnsignIn = findViewById(R.id.signIn);
+        counter = 3;
+
+        buttontnsignIn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+                if (userName.getText().toString().equals("admin") && password.getText().toString().equals("1234")) {
+                    Toast.makeText(getApplicationContext(), "UserName & Password Correct", Toast.LENGTH_LONG).show();
+                    return;
+                    //correct password
+                } else {
+                    counter--;
+                    Toast.makeText(getApplicationContext(), "UserName & Password Incorrect", Toast.LENGTH_LONG).show();
+
+                    if (counter==0){
+                        Toast.makeText(getApplicationContext(), "Application Login Blocked", Toast.LENGTH_LONG).show();
+                        buttontnsignIn.setClickable(false);
+
+                        buttontnsignIn.setBackground(Drawable.createFromPath("btn_not_clickable_style"));
+                    }
+                    //wrong password
+                }
+            }
+        });
 
     }
 
@@ -34,23 +60,22 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void signIn(View view) {
-        if (userName.getText().toString().equals("admin") && password.getText().toString().equals("1234")) {
-            Toast toast = Toast.makeText(getApplicationContext(), "UserName & Password Correct", Toast.LENGTH_LONG);
-            toast.show();
-            //correct password
-        } else {
-            counter--;
-            Toast toast = Toast.makeText(getApplicationContext(), "UserName & Password Incorrect", Toast.LENGTH_LONG);
-            toast.show();
-            if (counter==0){
-                Toast toast2 = Toast.makeText(getApplicationContext(), "Application Login Blocked", Toast.LENGTH_LONG);
-                toast2.show();
-                signIn.setClickable(false);
-
-                signIn.setBackground(Drawable.createFromPath("btn_not_clickable_style"));
-            }
-            //wrong password
-        }
-    }
+//    public void signIn(View view) {
+//        if (userName.getText().toString().equals("admin") && password.getText().toString().equals("1234")) {
+//            Toast.makeText(getApplicationContext(), "UserName & Password Correct", Toast.LENGTH_LONG).show();
+//            return;
+//            //correct password
+//        } else {
+//            counter--;
+//            Toast.makeText(getApplicationContext(), "UserName & Password Incorrect", Toast.LENGTH_LONG).show();
+//
+//            if (counter==0){
+//                Toast.makeText(getApplicationContext(), "Application Login Blocked", Toast.LENGTH_LONG).show();
+//                signIn.setClickable(false);
+//
+//                signIn.setBackground(Drawable.createFromPath("btn_not_clickable_style"));
+//            }
+//            //wrong password
+//        }
+//    }
 }
