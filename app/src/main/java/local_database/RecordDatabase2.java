9a4e8bce6add0f35c667277f3d8edd32709de0e5
Idan <idanbar93@gmail.com>
2019-09-16 17:32:04 +0300

    *UPDATE database uses only one table, record_table contains all user records

--- app/src/main/java/local_database/RecordDatabase2.java
+++ app/src/main/java/local_database/RecordDatabase2.java
@@ -3,41 +3,13 @@ package local_database;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-import com.securevault19.securevault2019.record.BankAccount;
-import com.securevault19.securevault2019.record.CreditCard;
-import com.securevault19.securevault2019.record.Cryptocurrency;
-import com.securevault19.securevault2019.record.DrivingLicence;
-import com.securevault19.securevault2019.record.Email;
-import com.securevault19.securevault2019.record.OnlineShoppingApp;
-import com.securevault19.securevault2019.record.Passport;
-import com.securevault19.securevault2019.record.SocialMediaApp;
-import com.securevault19.securevault2019.record.Website;
+import com.securevault19.securevault2019.record.Record;
 import com.securevault19.securevault2019.user.User;
 
-import local_database.dao.DaoBankAccount;
-import local_database.dao.DaoCreditCard;
-import local_database.dao.DaoCryptocurrency;
-import local_database.dao.DaoDrivingLicence;
-import local_database.dao.DaoEmail;
-import local_database.dao.DaoOnlineShoppingApp;
-import local_database.dao.DaoPassport;
-import local_database.dao.DaoSocialMediaApp;
-import local_database.dao.DaoUser;
-import local_database.dao.DaoWebsite;
+import local_database.dao.DaoRecord;
 
-@Database(entities = {User.class, BankAccount.class, CreditCard.class, Cryptocurrency.class, DrivingLicence.class, Email.class,
-        OnlineShoppingApp.class, Passport.class, SocialMediaApp.class, Website.class},version = 1)
+@Database(entities = {Record.class},version = 1)
 public abstract class RecordDatabase2 extends RoomDatabase {
 
-    public abstract DaoUser daoUser();
-    public abstract DaoBankAccount daoBankAccount();
-    public abstract DaoCreditCard daoCreditCard();
-    public abstract DaoCryptocurrency daoCryptocurrency();
-    public abstract DaoDrivingLicence daoDrivingLicence();
-    public abstract DaoEmail daoEmail();
-    public abstract DaoOnlineShoppingApp daoOnlineShoppingApp();
-    public abstract DaoPassport daoPassport();
-    public abstract DaoSocialMediaApp daoSocialMediaApp();
-    public abstract DaoWebsite daoWebsite();
-
+    public abstract DaoRecord daoRecord();
 }
--- app/src/main/java/repository/RecordRepository.java
+++ app/src/main/java/repository/RecordRepository.java
@@ -5,177 +5,71 @@ import android.os.AsyncTask;
 
 import androidx.lifecycle.LiveData;
 
-import com.securevault19.securevault2019.record.BankAccount;
-import com.securevault19.securevault2019.record.CreditCard;
-import com.securevault19.securevault2019.record.Cryptocurrency;
-import com.securevault19.securevault2019.record.DrivingLicence;
-import com.securevault19.securevault2019.record.Email;
-import com.securevault19.securevault2019.record.OnlineShoppingApp;
-import com.securevault19.securevault2019.record.Passport;
-import com.securevault19.securevault2019.record.SocialMediaApp;
-import com.securevault19.securevault2019.record.Website;
+import com.securevault19.securevault2019.record.Record;
 import com.securevault19.securevault2019.user.User;
 
 import java.util.List;
 
-import local_database.dao.DaoBankAccount;
-import local_database.dao.DaoCreditCard;
-import local_database.dao.DaoCryptocurrency;
-import local_database.dao.DaoDrivingLicence;
-import local_database.dao.DaoEmail;
-import local_database.dao.DaoOnlineShoppingApp;
-import local_database.dao.DaoPassport;
-import local_database.dao.DaoSocialMediaApp;
-import local_database.dao.DaoUser;
-import local_database.dao.DaoWebsite;
+import local_database.dao.DaoRecord;
+import local_database.dao.DaoRecord;
 import local_database.DatabaseClient;
 import local_database.RecordDatabase2;
 
 public class RecordRepository {
 
-    private DaoUser daoUser;
-    private LiveData<List<User>> allUserRecords;
-
-    private DaoBankAccount daoBankAccount;
-    private LiveData<List<BankAccount>> allBankAccountRecords;
-
-    private DaoCreditCard daoCreditCard;
-    private LiveData<List<CreditCard>> allCreditCardRecords;
-
-    private DaoCryptocurrency daoCryptocurrency;
-    private LiveData<List<Cryptocurrency>> allCryptocurrencyRecords;
-
-    private DaoDrivingLicence daoDrivingLicence;
-    private LiveData<List<DrivingLicence>> allDrivingLicenceRecords;
-
-    private DaoEmail daoEmail;
-    private LiveData<List<Email>> allEmailRecords;
-
-    private DaoOnlineShoppingApp daoOnlineShoppingApp;
-    private LiveData<List<OnlineShoppingApp>> allOnlineShoppingAppRecords;
-
-    private DaoPassport daoPassport;
-    private LiveData<List<Passport>> allPassportRecords;
-
-    private DaoSocialMediaApp daoSocialMediaApp;
-    private LiveData<List<SocialMediaApp>> allSocialMediaAppRecords;
-
-    private DaoWebsite daoWebsite;
-    private LiveData<List<Website>> allWebsiteRecords;
+    private DaoRecord DaoRecord;
+    private LiveData<List<Record>> allRecords;
 
 
     public RecordRepository(Application application) {
         RecordDatabase2 database2 = DatabaseClient.getInstance(application).getRecordDatabase2();      // getting the single ton database
 
-        daoBankAccount = database2.daoBankAccount();
-        allBankAccountRecords = daoBankAccount.getAllBankAccountRecords();
-
-        daoCreditCard = database2.daoCreditCard();
-        allCreditCardRecords = daoCreditCard.getAllCreditCardRecords();
-
-        daoCryptocurrency = database2.daoCryptocurrency();
-        allCryptocurrencyRecords = daoCryptocurrency.getAllCryptocurrencyRecords();
-
-        daoDrivingLicence = database2.daoDrivingLicence();
-        allDrivingLicenceRecords = daoDrivingLicence.getAllDrivingLicenceRecords();
-
-        daoEmail = database2.daoEmail();
-        allEmailRecords = daoEmail.getAllEmailRecords();
-
-        daoOnlineShoppingApp = database2.daoOnlineShoppingApp();
-        allOnlineShoppingAppRecords = daoOnlineShoppingApp.getAllOnlineShoppingAppRecords();
-
-        daoPassport = database2.daoPassport();
-        allPassportRecords = daoPassport.getAllPassportRecords();
-
-        daoSocialMediaApp = database2.daoSocialMediaApp();
-        allSocialMediaAppRecords = daoSocialMediaApp.getAllSocialMediaAppRecords();
-
-        daoUser = database2.daoUser();
-        allUserRecords = daoUser.getAllUserRecords();
-
-        daoWebsite = database2.daoWebsite();
-        allWebsiteRecords = daoWebsite.getAllWebsiteRecords();
+        DaoRecord = database2.daoRecord();
+        allRecords = DaoRecord.getAllRecords();
 
     }
 
-//    public void insert(Website website){
-//        new InsertWebsiteAsyncTask(daoWebsite).execute(website);
-//    }
-
-    public LiveData<List<User>> getAllUserRecords() {
-        return allUserRecords;
+    public void insert(Record record){
+        new InsertRecordAsyncTask(DaoRecord).execute(record);
     }
 
-public void show(Website website) {
-        new ShowWebsiteRecordAsyncTask(daoWebsite).execute(website);
-}
-    public LiveData<List<Website>> getAllWebsiteRecords() {
-        return allWebsiteRecords;
+    public LiveData<List<Record>> getAllRecords() {
+        return allRecords;
     }
 
-    public LiveData<List<BankAccount>> getAllBankAccountRecordsRecords() {
-        return allBankAccountRecords;
+    public void show(Record record) {
+        new ShowRecordAsyncTask(DaoRecord).execute(record);
     }
 
-    public LiveData<List<CreditCard>> getAllCreditCardRecords() {
-        return allCreditCardRecords;
-    }
+    private static class ShowRecordAsyncTask extends AsyncTask<Record, Void, Void> {
+        private DaoRecord DaoRecord;
 
-    public LiveData<List<Cryptocurrency>> getAllCryptocurrencyRecords() {
-        return allCryptocurrencyRecords;
-    }
-
-    public LiveData<List<DrivingLicence>> getAllDrivingLicenceRecords() {
-        return allDrivingLicenceRecords;
-    }
-
-    public LiveData<List<Email>> getAllEmailRecords() {
-        return allEmailRecords;
-    }
-
-    public LiveData<List<OnlineShoppingApp>> getAllOnlineShoppingAppRecords() {
-        return allOnlineShoppingAppRecords;
-    }
-
-    public LiveData<List<SocialMediaApp>> getAllSocialMediaAppRecords() {
-        return allSocialMediaAppRecords;
-    }
-
-    public LiveData<List<Passport>> getAllPassportRecords() {
-        return allPassportRecords;
-    }
-
-    private static class ShowWebsiteRecordAsyncTask extends AsyncTask<Website, Void, Void> {
-        private DaoWebsite daoWebsite;
-
-        private ShowWebsiteRecordAsyncTask(DaoWebsite daoWebsite) {
-            this.daoWebsite = daoWebsite;
+        private ShowRecordAsyncTask(DaoRecord daoRecord) {
+            this.DaoRecord = DaoRecord;
         }
 
         @Override
-        protected Void doInBackground(Website... websites) {
-            daoWebsite.delete(websites[0]);
+        protected Void doInBackground(Record... records) {
+            DaoRecord.delete(records[0]);
             return null;
         }
 
     }
 
+    private static class InsertRecordAsyncTask extends AsyncTask<Record, Void, Void> {
+        private DaoRecord daoRecord;
 
+        private InsertRecordAsyncTask(DaoRecord daoRecord) {
+            this.daoRecord = daoRecord;
+        } //Constructor because we cannot access the dao of the repository direcly.
+        // (because here it's a class by itself)
 
-//    private static class InsertWebsiteAsyncTask extends AsyncTask<Website, Void, Void> {
-//        private DaoWebsite daoWebsite;
-//
-//        private InsertWebsiteAsyncTask(DaoWebsite daoWebsite){
-//            this.daoWebsite = daoWebsite;
-//        } //Constructor because we cannot access the dao of the repository direcly.
-//          // (because here it's a class by itself)
-//
-//        @Override
-//        protected Void doInBackground(Website... websites) {
-//            daoWebsite.insert(websites[0]);
-//            return null;
-//        }
+        @Override
+        protected Void doInBackground(Record... records) {
+            daoRecord.insert(records[0]);
+            return null;
+        }
+    }
 }
 
 
