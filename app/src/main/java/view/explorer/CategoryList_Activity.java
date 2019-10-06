baef9cae0ed4764ef2afa1deb8240eff8f7945f2
Yotamsh2 <yotamsh2@gmail.com>
2019-10-06 21:30:12 +0300

    activities

--- app/src/main/java/view/explorer/CategoryList_Activity.java
+++ app/src/main/java/view/explorer/CategoryList_Activity.java
@@ -152,6 +152,8 @@ public class CategoryList_Activity extends AppCompatActivity {
             this.startActivity(intent);
             overridePendingTransition(0, 0);
             finish();
+            overridePendingTransition(0, 0);
+
         }
     }
 
