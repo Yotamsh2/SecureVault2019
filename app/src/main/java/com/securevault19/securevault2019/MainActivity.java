a26d31209a8d20fdb35ee57cb6151b8b1e3c545b
Ivgesha <evgenise93@gmail.com>
2019-10-21 04:28:53 +0300

    DaoUser created UserRepository created Added reading from DataBase when we try to LogIn.
    To continue:
    The User Details are NOT encrypted.
    
    FIXED:
    App dosent crash if the user dosent exists.
    
    ERRORS & BUGS:
    1. Different accounts have the same TABLES.
    User A can use Users B tables and info!
    2. the behavior of the app when there is no user name in the table and a user name with wrond password, is the same!
    
    SOLUTIONS
    1. Add to the creation of the table "DataBaseClient2" + user.getFirstName.
    the FirstName is primary key so we can use it to build unique TABLES
    2. tried to fix with flag.
    dosent work.
    maybe need to consider to change the DaoUser QUERY.
    BUT! i think its ok that they behave theSame!

--- app/src/main/java/com/securevault19/securevault2019/MainActivity.java
+++ app/src/main/java/com/securevault19/securevault2019/MainActivity.java
@@ -30,7 +30,8 @@ public class MainActivity extends AppCompatActivity {
     private int counter;
     // added for testing //
     private User user;
-    private int flag =0;
+    private int flag = 0;
+
     //                  //
     @Override
     protected void onCreate(Bundle saveBtndInstanceState) {
@@ -61,17 +62,6 @@ public class MainActivity extends AppCompatActivity {
         logo.startAnimation(animation1);
         signInForm.startAnimation(animation2);
 
-
-//        buttonSignIn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                signIn(userName, password, buttonSignIn);
-//                buttonSignIn.startAnimation(animation3);
-//                mediaPlayer.start();
-//            }
-//        });
-//
-
     }
 
     public void signUp(View view) {
@@ -83,88 +73,16 @@ public class MainActivity extends AppCompatActivity {
     public void forgotPass(View view) {
 
     }
-//
-//    @SuppressLint("StaticFieldLeak")
-//    public void signIn(final EditText userName, final EditText password, Button buttonSignIn) {
-//        final String firstName = userName.getText().toString();
-//        final String masterPassword = password.getText().toString();
-//
-//
-//        // the AsyncTask is crucial! Without it, we will get MEMORY LEAK!
-//        new AsyncTask<User, Void, Void>() {
-//
-//
-//            @SuppressLint("WrongThread")
-//            @Override
-//            protected Void doInBackground(User... users) {
-//                String firstName = userName.getText().toString();
-//                String masterPassword = password.getText().toString();
-//                Log.e("check1", "" + firstName + " " + masterPassword);
-//                User test = DatabaseClient.getInstance(getApplication()).getRecordDatabase2().daoUser().LogInConfirmation(firstName, masterPassword);
-//                Log.e("check1", "" + test.toString());
-//                Log.e("check1", "" + test.getFirstName());
-//
-////                if (firstName.equals(test.getFirstName()) && masterPassword.equals(test.getMasterPassword())) {
-////
-////
-////                }
-//
-//                return null;
-//
-//            }
-//
-//            @Override
-//            protected void onPostExecute(Void aVoid) {
-//                super.onPostExecute(aVoid);
-//                User test = DatabaseClient.getInstance(getApplication()).getRecordDatabase2().daoUser().LogInConfirmation(firstName, masterPassword);
-//                if (firstName.equals(test.getFirstName()) && masterPassword.equals(test.getMasterPassword())) {
-//                    finish();
-//                }
-//                else
-//
-//            }
-//        }.execute();
-//
-//
-//
-//        //boolean test = DatabaseClient.getInstance(getApplication()).getRecordDatabase2().daoUser().LogInConfirmation(firstName, masterPassword);
-//        //Log.e("check1", "" + test);
-//
-//
-//    }
-//        if (userName.getText().toString().equals("A") && password.getText().toString().equals("1")) {
-//            //Checking if the user wants a pattern password:
-//            if (true) {
-//                Intent intent = new Intent(this, PatternLockView_Activity.class);
-//                this.startActivity(intent);
-//                //overridePendingTransition(R.anim.fadein_fast, 0);
-//                finish();
-//            } else {
-//                Intent intent = new Intent(this, CategoryList_Activity.class);
-//                this.startActivity(intent);
-//                finish();
-//            }
-//        }
-//        else{
-//            counter--;
-//            Toast.makeText(getApplicationContext(), "UserName & Password Incorrect" + "\n" + "Still Have " + counter + " To Try", Toast.LENGTH_LONG).show();
-//
-//            if (counter == 0) {
-//                Toast.makeText(getApplicationContext(), "Application Login Blocked", Toast.LENGTH_LONG).show();
-//                buttonSignIn.setClickable(false);
-//
-//                buttonSignIn.setBackground(Drawable.createFromPath("btn_not_clickable_style"));
-//            }
-//            //wrong password
-//        }
-//
-//    }
 
-
+    // At this onClick method
+    // we are checking if the user is exists .
+    // if he is, great
+    // if the user name correct and pass not, incorrect
+    // if the user dosent exist, incorrect
+    // works the same as user name correct and pass not.
     @SuppressLint("StaticFieldLeak")
     public void signIn(View view) {
 
-        Toast.makeText(getApplicationContext(), " clicked ", Toast.LENGTH_LONG).show();
 
         final EditText userName = findViewById(R.id.userName);
         final EditText password = findViewById(R.id.password_EditText);
@@ -172,63 +90,51 @@ public class MainActivity extends AppCompatActivity {
         final String masterPassword = password.getText().toString();
         Log.e("check2", " " + firstName + " " + masterPassword);
         //user =  new User("Test", null, null, null, null, null, null, null);
-        Log.e("test2","" +flag);
+        Log.e("test2", "" + flag);
 
-        new AsyncTask<User,Void,Void>(){
+        new AsyncTask<User, Void, Void>() {
             @Override
             protected void onPreExecute() {
                 super.onPreExecute();
-                Log.e("test2","" +flag);
+                Log.e("test2", "" + flag);
             }
 
             @Override
             protected Void doInBackground(User... users) {
+                Log.e("phase1", "Before reading From DATABASE");
                 user = DatabaseClient.getInstance(getApplication()).getRecordDatabase2().daoUser().LogInConfirmation(firstName, masterPassword);
-                if ((firstName.equals(user.getFirstName())) && (masterPassword.equals(user.getMasterPassword()))) {
-                    Log.e("test2","firstName: "+ firstName + " User.GetFirstName: " + user.getFirstName() + " masterPassword: "+ masterPassword + " user.getMasterPassword():" + user.getMasterPassword());
-                    flag =1;
+
+
+                if (user != null) {
+                    if ((firstName.equals(user.getFirstName())) && (masterPassword.equals(user.getMasterPassword()))) {
+                        Log.e("phase1", "entered if statment");
+                        Log.e("test2", "firstName: " + firstName + " User.GetFirstName: " + user.getFirstName() + " masterPassword: " + masterPassword + " user.getMasterPassword():" + user.getMasterPassword());
+                        flag = 1;
+                    } else {
+                        Log.e("phase1", "reached else statment");
+                        //flag = 2;
+                    }
+                    Log.e("phase1", "before return statment");
+
                 }
-                else
-                    flag =2;
                 return null;
             }
+
 
             @Override
             protected void onPostExecute(Void aVoid) {
                 super.onPostExecute(aVoid);
-                if(flag ==1 ){
+                if (flag == 1) {
                     //  Toast.makeText(getApplicationContext(), " flag = " + flag, Toast.LENGTH_LONG).show();
-                    Intent intent= new Intent();
-                    intent.setClass(getApplicationContext(),CategoryList_Activity.class);
+                    Intent intent = new Intent();
+                    intent.setClass(getApplicationContext(), CategoryList_Activity.class);
                     startActivity(intent);
+                } else {
+                    Toast.makeText(getApplicationContext(), " flag = " + flag, Toast.LENGTH_LONG).show();
                 }
-                else
-                    return;
+
             }
         }.execute();
 
-
-
-
-//
-
-
-//        if (firstName.equals(user[0].getFirstName()) && masterPassword.equals(user[0].getMasterPassword())) {
-//            Toast.makeText(getApplicationContext(), " if worked ", Toast.LENGTH_LONG).show();
-//        }
-//        else
-//            Toast.makeText(getApplicationContext(), " if DOSENT worked ", Toast.LENGTH_LONG).show();
-
-//                User user = DatabaseClient.getInstance(getApplication()).getRecordDatabase2().daoUser().LogInConfirmation(firstName, masterPassword);
-//        if (firstName.equals(test.getFirstName()) && masterPassword.equals(test.getMasterPassword())) {
-//            Log.e("check2", "" + test.getFirstName() + " " + test.getMasterPassword());
-//            Intent intent = new Intent(this, CategoryList_Activity.class);
-//            this.startActivity(intent);
-//        } else
-//            Toast.makeText(getApplicationContext(), "Wrong UserName OR PassWord", Toast.LENGTH_LONG).show();
-//
-//        return;
     }
-
-
 }
