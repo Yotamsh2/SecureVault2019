26d12e2d6cd31ccc9815ac34cba715c31938218f
Ivgesha <evgenise93@gmail.com>
2019-10-29 14:34:37 +0200

    Merge branch 'master' of E:\android studio projects\SecureVault2019 with conflicts.

--- app/src/main/java/com/securevault19/securevault2019/MainActivity.java
+++ app/src/main/java/com/securevault19/securevault2019/MainActivity.java
@@ -2,9 +2,10 @@ package com.securevault19.securevault2019;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -16,24 +17,31 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.securevault19.securevault2019.user.User;
+
+import local_database.DatabaseClient;
+import view.entrance.SignUp_Activity;
 import view.explorer.CategoryList_Activity;
-import view.explorer.PatternLockView_Activity;
 
 
 @SuppressLint("Registered")
 public class MainActivity extends AppCompatActivity {
 
     private int counter;
+    // added for testing //
+    private User user;
+    private int flag = 0;
 
+    //                  //
     @Override
     protected void onCreate(Bundle saveBtndInstanceState) {
         super.onCreate(saveBtndInstanceState);
         setContentView(R.layout.activity_main);
 
-        final MediaPlayer mediaPlayer = MediaPlayer.create(this,R.raw.button);
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.button);
         ImageView logo = findViewById(R.id.logo);
-        Button forgotPass =  findViewById((R.id.forgotPass));
-        Button signUp =  findViewById(R.id.signUp);
+        Button forgotPass = findViewById((R.id.forgotPass));
+        Button signUp = findViewById(R.id.signUp);
         LinearLayout signInForm = findViewById(R.id.signInForm);
         final EditText userName = findViewById(R.id.userName);
         final EditText password = findViewById(R.id.password_EditText);
@@ -41,68 +49,98 @@ public class MainActivity extends AppCompatActivity {
         counter = 3;
 
 
-    // for testing //
-    userName.setText("A");
-    password.setText("1");
-    //------------//
+        // for testing //
+        userName.setText("A");
+        password.setText("1");
+        //------------//
 
 
         //Animation Sets
-        Animation animation1 = AnimationUtils.loadAnimation(MainActivity.this,R.anim.zoomin);
-        final Animation animation2 = AnimationUtils.loadAnimation(MainActivity.this,R.anim.bottomtotop);
-        final Animation animation3 = AnimationUtils.loadAnimation(MainActivity.this,R.anim.buttonpush_anim);
+        Animation animation1 = AnimationUtils.loadAnimation(MainActivity.this, R.anim.zoomin);
+        final Animation animation2 = AnimationUtils.loadAnimation(MainActivity.this, R.anim.bottomtotop);
+        final Animation animation3 = AnimationUtils.loadAnimation(MainActivity.this, R.anim.buttonpush_anim);
         logo.startAnimation(animation1);
         signInForm.startAnimation(animation2);
-
-
-        buttonSignIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                signIn(userName, password, buttonSignIn);
-                buttonSignIn.startAnimation(animation3);
-                mediaPlayer.start();
-            }
-        });
-
 
 
     }
 
     public void signUp(View view) {
 
+        Intent intent = new Intent(this, SignUp_Activity.class);
+        this.startActivity(intent);
     }
-    public void forgotPass(View view){
+
+    public void forgotPass(View view) {
 
     }
 
-    public void signIn(EditText userName, EditText password, Button buttonSignIn) {
-        if (userName.getText().toString().equals("A") && password.getText().toString().equals("1")) {
-            //Checking if the user wants a pattern password:
-            if(true){
-                Intent intent = new Intent(this, PatternLockView_Activity.class);
-                this.startActivity(intent);
-                //overridePendingTransition(R.anim.fadein_fast, 0);
-                finish();
+    // At this onClick method
+    // we are checking if the user is exists .
+    // if he is, great
+    // if the user name correct and pass not, incorrect
+    // if the user dosent exist, incorrect
+    // works the same as user name correct and pass not.
+    @SuppressLint("StaticFieldLeak")
+    public void signIn(View view) {
+
+
+        final EditText userName = findViewById(R.id.userName);
+        final EditText password = findViewById(R.id.password_EditText);
+        final String firstName = userName.getText().toString();
+        final String masterPassword = password.getText().toString();
+        Log.e("check2", " " + firstName + " " + masterPassword);
+        //user =  new User("Test", null, null, null, null, null, null, null);
+        Log.e("test2", "" + flag);
+
+        new AsyncTask<User, Void, Void>() {
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                Log.e("test2", "" + flag);
             }
-            else{
-                Intent intent = new Intent(this, CategoryList_Activity.class);
-                this.startActivity(intent);
-                finish();
+
+            @Override
+            protected Void doInBackground(User... users) {
+                Log.e("phase1", "Before reading From DATABASE");
+
+                // searching for user //
+                user = DatabaseClient.getInstance(getApplication()).getRecordDatabase2().daoUser().LogInConfirmation(firstName, masterPassword);
+                // ------------------ //
+
+
+                if (user != null) {  // if user found.
+                    if ((firstName.equals(user.getFirstName())) && (masterPassword.equals(user.getMasterPassword()))) {
+                        Log.e("phase1", "entered if statment");
+                        Log.e("test2", "firstName: " + firstName + " User.GetFirstName: " + user.getFirstName() + " masterPassword: " + masterPassword + " user.getMasterPassword():" + user.getMasterPassword());
+                        flag = 1;
+                    } else {
+                        // one of the params are not correct
+                        Log.e("phase1", "reached else statment");
+                        //flag = 2;
+                    }
+                    Log.e("phase1", "before return statment");
+
+                }
+                return null;
             }
 
 
-        } else {
-            counter--;
-            Toast.makeText(getApplicationContext(), "UserName & Password Incorrect"+"\n"+"Still Have "+counter+" To Try", Toast.LENGTH_LONG).show();
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                if (flag == 1) {
+                    //  Toast.makeText(getApplicationContext(), " flag = " + flag, Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent();
+                    intent.setClass(getApplicationContext(), CategoryList_Activity.class);
+                    startActivity(intent);
+                } else {
+                    //Toast.makeText(getApplicationContext(), " flag = " + flag, Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "Wrong UserName or Password", Toast.LENGTH_LONG).show();
+                }
 
-            if (counter == 0) {
-                Toast.makeText(getApplicationContext(), "Application Login Blocked", Toast.LENGTH_LONG).show();
-                buttonSignIn.setClickable(false);
-
-                buttonSignIn.setBackground(Drawable.createFromPath("btn_not_clickable_style"));
             }
-            //wrong password
-        }
+        }.execute();
 
     }
 }
